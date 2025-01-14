a556b03d588a831899a6f32f3716066766d2c15a
Md. Sumon Islam <sumonst21@gmail.com>
2025-01-15 02:43:54 +0600

    Update meilisearch-wordpress.php

--- meilisearch-wordpress.php
+++ meilisearch-wordpress.php
@@ -3,10 +3,11 @@
     Plugin Name: Meilisearch Wordpress
     Plugin URI: https://wordpress.meilisearch.dev
     description: The best search experience in wordpress with Meilisearch
-    Version: 0.1.0
+    Version: 0.1.1
     Author: Meilisearch
     Author URI: https://meilisearch.com
     License: MIT
+    Text Domain: meilisearch-wordpress
     */
 
     require_once __DIR__ . '/vendor/autoload.php';
@@ -27,5 +28,26 @@
 
     if ( is_admin() )
         $meilisearch = new MeiliSearch();
+
+        $meilisearch_options = get_option( 'meilisearch_option_name' );
+        $meilisearch_url = $meilisearch_options['meilisearch_url_0'];
+        $meilisearch_search_url = $meilisearch_options['meilisearch_search_url_4'];
+        $meilisearch_public_key = $meilisearch_options['meilisearch_public_key_2'];
+        $meilisearch_index_name = $meilisearch_options['meilisearch_index_name'];
+
+        // Tell the admin to configure the plugin before using it
+        if (!isset($meilisearch_options['meilisearch_url_0']) || !isset($meilisearch_options['meilisearch_private_key_1'])){
+            add_action('admin_notices', 'meilisearch_admin_notice');
+        }
+
+        function meilisearch_admin_notice(){
+            ?>
+            <div class="notice notice-error is-dismissible">
+                <p><?php _e( 'Meilisearch is not configured. Please configure it in the Meilisearch settings page.', 'meilisearch-wordpress' ); ?></p>
+            </div>
+            <?php
+        }
+
+
 
 ?>
